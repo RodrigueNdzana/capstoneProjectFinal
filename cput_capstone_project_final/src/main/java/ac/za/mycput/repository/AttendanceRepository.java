bd6a5e614cc5bd77c81febabed465d0a150d4f80
RodrigueNdzana <54138415+RodrigueNdzana@users.noreply.github.com>
2023-10-21 14:41:41 +0200

    started attendance

--- cput_capstone_project_final/src/main/java/ac/za/mycput/repository/AttendanceRepository.java
+++
@@ -1,9 +0,0 @@
-package ac.za.mycput.repository;
-
-import ac.za.mycput.entity.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
-}
